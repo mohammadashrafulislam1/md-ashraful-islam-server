15cedd88e1c7ea85590b119bd26ad1bb8cb82f73
mohammadashrafulislam <ixdell41@gmail.com>
2024-04-08 02:07:54 +0600

    upadeted cloudinary

--- routes/project.js
+++ routes/project.js
@@ -1,6 +1,12 @@
 import express from "express";
 import { addProject } from "../controllers/projectController.js";
+import { upload } from "../middleware/multer.js";
 
 export const projectRouter = express.Router();
 
-projectRouter.post("/", addProject)
+projectRouter.post("/", upload.fields([
+    { name: 'galleryImages', maxCount: 5 },
+    { name: 'projectImage', maxCount: 1 },
+    { name: 'mobileImage', maxCount: 1 },
+    { name: 'tabletImage', maxCount: 1 }
+  ]), addProject)
