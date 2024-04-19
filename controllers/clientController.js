9120186f0403e575bac1bfba4645e78810741595
mohammadashrafulislam <ixdell41@gmail.com>
2024-04-19 09:24:52 +0600

    get single client by ID controller

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import { clientModel } from "../model/Clients.js";
 
 export const addClient = async(req, res)=>{
@@ -19,4 +20,19 @@ export const addClient = async(req, res)=>{
         console.error("Error adding client:", error);
         res.status(500).json({ error: "Internal Server Error" });
       }
+}
+// get single client
+export const getOneClient = async (req, res)=>{
+  try{
+    const clientId =req.params.id;
+  const client = await clientModel.findOne({_id: new ObjectId(clientId)});
+  if (client) {
+    res.json(client);
+  } else {
+    res.status(404).json({ message: 'Client not found' });
+  }
+  }
+  catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
