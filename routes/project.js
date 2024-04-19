7c3545518d5bce5c765a947367df999ebabb1d65
mohammadashrafulislam <ixdell41@gmail.com>
2024-04-19 09:10:27 +0600

    get products router

--- routes/project.js
+++ routes/project.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { addProject } from "../controllers/projectController.js";
+import { addProject, getProjects } from "../controllers/projectController.js";
 import { upload } from "../middleware/multer.js";
 export const projectRouter = express.Router();
-
+// add project router
 projectRouter.post("/", upload.fields([
   { name: 'galleryImages', maxCount: 5 }
 ]), addProject);
+// get project router
+projectRouter.get("/", getProjects)
