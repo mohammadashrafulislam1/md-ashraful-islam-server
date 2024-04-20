b84b25512b4c5434232399c962c7e2c6924cafcc
mohammadashrafulislam <ixdell41@gmail.com>
2024-04-20 09:38:36 +0600

    added update project router

--- routes/project.js
+++ routes/project.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { addProject, deleteProject, getProjects } from "../controllers/projectController.js";
+import { addProject, deleteProject, getProjects, updateProjectController } from "../controllers/projectController.js";
 import { upload } from "../middleware/multer.js";
 export const projectRouter = express.Router();
+
 // add project router
 projectRouter.post("/", upload.fields([
   { name: 'galleryImages', maxCount: 5 }
@@ -10,3 +11,5 @@ projectRouter.post("/", upload.fields([
 projectRouter.get("/", getProjects)
 // delete project router
 projectRouter.delete("/:id", deleteProject)
+// update project router
+projectRouter.put("/:id", updateProjectController)
