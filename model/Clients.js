c98d2ce2fb02a64a218edad005c6324ab4e0096b
mohammadashrafulislam <ixdell41@gmail.com>
2024-04-21 09:33:42 +0600

    Clietn model updated for new field

--- model/Clients.js
+++ model/Clients.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const clientSchema = new mongoose.Schema({
     clientName: { type: String},
+    clientPhoto:{type: String},
     clientEmail: { type: String },
     clientSocialMedia: { type: String }
   });
