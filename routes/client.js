518108b211f64f27934a38cd5a11f90a8cba6fef
mohammadashrafulislam <ixdell41@gmail.com>
2024-04-21 09:35:46 +0600

    changes on client router for clientImage

--- routes/client.js
+++ routes/client.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { addClient, deleteClient, getClients, getOneClient } from "../controllers/clientController.js";
+import { upload } from "../middleware/multer.js";
 
 export const clientRouter = express.Router();
 
 // add client (only work separately)
-clientRouter.post('/', addClient)
+clientRouter.post('/', upload.single('clientPhoto'), addClient)
 // get single client
 clientRouter.get('/:id', getOneClient)
 // get all clients 
