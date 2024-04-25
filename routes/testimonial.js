1ad5028cf0df8e7e73dbbfe0355b6f2e21d6412e
mohammadashrafulislam <ixdell41@gmail.com>
2024-04-25 16:32:53 +0600

    testimonial router

--- routes/testimonial.js
+++ routes/testimonial.js
@@ -1,4 +1,8 @@
 import express from "express";
+import { upload } from "../middleware/multer";
+import { addTestimonial } from "../controllers/testimonialController";
 
 
 export const testRouter = express.Router();
+
+testRouter.post('/', upload.single('image'), addTestimonial)
