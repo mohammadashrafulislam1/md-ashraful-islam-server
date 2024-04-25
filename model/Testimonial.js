342d8a469d10817ab0d8912a44115288fdfb5281
mohammadashrafulislam <ixdell41@gmail.com>
2024-04-25 16:15:40 +0600

    Testimonial Model

---
+++ model/Testimonial.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const testimonialSchema = new mongoose.Schema({
+    image: {type: String},
+    name:{type: String},
+    email:{type:String},
+    testimonial:{type : String},
+    rating:{type: Number},
+    des: {type: String},
+    socialMedia: {type: String},
+    isActive:{type: Boolean, default: false}
+})
