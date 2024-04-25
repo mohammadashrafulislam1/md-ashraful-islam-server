dcc99c3eac7a3bcfeb3a5b604e2bb4e6ef8ab768
mohammadashrafulislam <ixdell41@gmail.com>
2024-04-25 16:27:15 +0600

    testimonial controller is added

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,6 +1,18 @@
 export const addTestimonial = async(req, res) =>{
     try{
         const imageResult = await cloudinary.uploader.upload(req.file.path);
+      const testimonial = new testimonialModel({
+        image: imageResult.secure_url,
+        name: req.body.name,
+        email: req.body.email,
+        testimonial: req.body.testimonial,
+        rating: req.body.rating,
+        des: req.body.des,
+        socialMedia: req.body.socialMedia,
+        isActive: req.body.isActive
+      })
+      const insert = await testimonial.save();
+      res.status(201).json(insert);
     }
     catch (e){
             // Handle error
