f611a99877cc2b0835a6f3be8d56d1dd55dd67d5
mohammadashrafulislam <ixdell41@gmail.com>
2024-04-25 18:51:11 +0600

    fixed error and working get API

--- routes/testimonial.js
+++ routes/testimonial.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { upload } from "../middleware/multer.js";
-import { addTestimonial } from "../controllers/testimonialController.js";
+import { addTestimonial, getTestimonials } from "../controllers/testimonialController.js";
 
 
 export const testRouter = express.Router();
 
 testRouter.post('/', upload.single('image'), addTestimonial)
+testRouter.get('/', getTestimonials)
